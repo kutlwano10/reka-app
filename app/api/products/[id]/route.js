5c0290f01277c0cdf7d652005dfb7ece1b0ee930
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-15 16:37:47 +0200

    i fixed a bug on PUT({params}) , i had to distracture the params, hence it didnt update the new details of product

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -2,10 +2,10 @@ import connectToMongoDB from "@/libs/mongodb"
 import Product from "@/models/product"
 import { NextResponse } from "next/server"
 
-export async function PUT(request,params) {
+export async function PUT(request,{params}) {
     const {id} = params
     const {newTitle : title, newDescription: description} =await request.json()
     await connectToMongoDB()
     await Product.findByIdAndUpdate(id, {title, description})
-    return NextResponse.json({message: "Product Updated"}, {status:2001})
+    return NextResponse.json({message: "Product Updated"}, {status:201})
 }

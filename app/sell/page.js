367da11d128af3310cf274cb679a08cad663454a
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-18 12:54:14 +0200

    created a branch

--- app/sell/page.js
+++ app/sell/page.js
@@ -11,8 +11,9 @@ const sell = () => {
         class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
         
       >
-        Sign in with google
+        Sign in with google Now
       </button>
+      
     </div>
   );
 };

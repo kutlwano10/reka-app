49b36024c529ae98cbe52fbd19a1fcb01321aa79
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-18 13:09:53 +0200

    page.js file commit

--- app/sell/page.js
+++ app/sell/page.js
@@ -8,7 +8,7 @@ const sell = () => {
       </h1>
       <p className="font-medium my-4">An Account is needed to view this page</p>
       <button
-        class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+        className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
         
       >
         Sign in with google Now

4e57bbfa3f7072ebb813420bcd4cab24d6e6f618
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-30 22:14:47 +0200

    i added pwa compatability

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,28 @@
 import { Providers } from "./GlobalRedux/Provider";
 import "./globals.css";
-import { CartProvider } from './CartContext';
+import { CartProvider } from "./CartContext";
 import Header from "./components/Header";
 
-// import Footer from "./components/Footer";
-
 export const metadata = {
-  title: "Reka online market",
-  description: "Fresh food delivered to Your Door step",
+  title: 'Reka Store',
+  description: 'A marketplace for farmers to sell products online',
+  icons: {
+    icon: '/favicon.ico', // Replace with your actual favicon
+  },
+  manifest: '/manifest.json', // This points to your PWA manifest file
+   // Sets the theme color for PWA
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body>
         <Providers>
-        <CartProvider>
+          <CartProvider>
             <Header className="fixed top-0 w-full z-10" /> {/* Fixed header */}
-            <main className="pt-">  {children} </main>
-         </CartProvider>
+            <main className="pt-"> {children} </main>
+          </CartProvider>
         </Providers>
       </body>
     </html>

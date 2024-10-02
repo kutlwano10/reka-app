277779f5e3066277a78664a4cd6e7f72fe6df5a0
Kitso Mogale <kitsomogale19@gmail.com>
2024-10-02 11:39:53 +0200

    skeletonCard

--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -14,7 +14,7 @@ const ProductList = () => {
       // console.log('123')
       try {
        
-        const response = await fetch(`https://reka-app-three.vercel.app/api/products/?category=${filter}&search=${search}`, { cache: "no-store" });
+        const response = await fetch(`http://localhost:3000/api/products/?category=${filter}&search=${search}`, { cache: "no-store" });
         if (!response.ok) {
           throw new Error("Failed to fetch products");
           
@@ -32,7 +32,8 @@ const ProductList = () => {
     <>
     <div className="px-[2%] md:px-0 max-w-xl md:mx-auto grid gap-4 grid-cols-2 lg:grid-cols-5 justify-center md:grid-cols-3 lg:mx-[9%] items-center lg:max-w-none my-4">
       {/* Show skeleton loaders when products are still loading */}
-      {products== [] ? (
+      {console.log(products)}
+      {products.length ==0? (
         // Render 10 skeleton loaders to match the grid layout
         Array(10).fill(0).map((_, idx) => <SkeletonLoader key={idx} />)
       ) : (
